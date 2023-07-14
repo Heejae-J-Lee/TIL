b2cd58b02127846241da7327d557e979113d9aaa
Heejae Lee <hee.404.jerry@gmail.com>
2023-07-15 01:24:48 +0900

    [IOS] Udemy : FlashChat

---
+++ Udemy/Flash-Chat/Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -0,0 +1,38 @@
+//
+//  WelcomeViewController.swift
+//  Flash Chat iOS13
+//
+//  Created by Angela Yu on 21/10/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let titleText = "⚡️FlashChat"
+        titleLabel.text = ""
+        var charIndex = 0
+        for letter in titleText {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * Double(charIndex), repeats: false) { (timer) in
+                self.titleLabel.text?.append(letter)
+            }
+            charIndex += 1
+        }
+       
+    }
+    
+    @IBAction func registerButtonPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: "goToRegister", sender: self)
+    }
+    
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: "goToLogin", sender: self)
+    }
+    
+}
