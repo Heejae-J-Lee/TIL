1f2238f8034d2851cb271be822a85623fc475222
Heejae Lee <hee.404.jerry@gmail.com>
2023-07-29 00:09:21 +0900

    [IOS] Udemy : Swift UI 기초
    I am Rich SwiftUI로 구성하기

---
+++ Udemy/I Am Rich UI/I Am Rich UI/I_Am_Rich_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_Rich_UIApp.swift
+//  I Am Rich UI
+//
+//  Created by Heejae on 2023/07/28.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_Rich_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
