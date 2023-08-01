58b178b159c13938f3a72774ae3f8d21544c83d5
Heejae Lee <hee.404.jerry@gmail.com>
2023-08-01 23:32:22 +0900

    [IOS] Udemy : Dicee : SwiftUI

---
+++ Udemy/Dices-SwiftUI/Dices-SwiftUI/Dices_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dices_SwiftUIApp.swift
+//  Dices-SwiftUI
+//
+//  Created by Heejae on 2023/08/01.
+//
+
+import SwiftUI
+
+@main
+struct Dices_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
